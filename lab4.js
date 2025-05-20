ff88a81d008c0a2349937c521e8a33dd87b7d158
sadlymarmar <129472971+sadlymarmar@users.noreply.github.com>
2025-05-19 23:06:59 -0400

    Add files via upload

---
+++ lab4.js
@@ -0,0 +1,7 @@
+function changeColor() {
+  if(document.getElementById("color-change-btn").style.color == 'red') {
+      document.getElementById("color-change-btn").style.color = 'black'
+  } else {
+      document.getElementById("color-change-btn").style.color = 'red'
+  }
+}
